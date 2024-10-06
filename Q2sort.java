73d41c2e5a94b7660f714d8d6a82d22de1d2b4a8
KANITA VERMA <vermakanita13@gmail.com>
2024-10-06 21:22:49 +0530

    Add files via upload

---
+++ Q2sort.java
@@ -0,0 +1,29 @@
+
+
+  //Q2sort array 
+//import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Q2sort{
+public static void main(String args[]){
+ArrayList <String> s = new ArrayList<>();
+s.add("indor");
+s.add("khargone");
+s.add("maheshwar");
+s.add("delhi");
+s.add("gujrat");
+System.out.println(s);
+
+Collections.sort(s);
+
+for(String j:s){
+System.out.println(j);
+}}
+}
+
+
+
+
+
